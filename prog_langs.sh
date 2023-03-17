237bb86950a602cb999683fd4d56f799c9d8b270
Robert Paulsen <ozymandias.1987@protonmail.com>
2023-03-17 13:41:47 -0600

    Deleted Repeated directories, updated go pkg version, rust headless install too

--- prog_langs.sh
+++ prog_langs.sh
@@ -1,38 +1,40 @@
 #!/usr/bin/bash
 
-# Scripts to auto download, create direcotries, setup and config all in one,  bash script Golang, rustlang, cargo, nvm, npm, node,yarn, and other tools and pkg.
+# Scripts to auto download, create direcotries, setup and config all in one,  bash script Golang, rustlang, cargo, nvm, npm, node, yarn and other tools and pkges.
 
 # Rust and Cargo unattended isntallation
 cd &&
-curl https://sh.rustup.rs -sSf | sh -s -- --profile default --no-modify-path --default-toolchain nightly rustup-managed toolchain nightly &&
+curl https://sh.rustup.rs -sSf | sh -s -- --profile default --no-modify-path --default-toolchain nightly &&
+# curl https://sh.rustup.rs -sSf | sh -s -- --profile default --no-modify-path --default-toolchain nightly toolchain nightly &&
 
 # Adding Cargo tu environment and then setting up local cargo call
-export PATH=$PATH:$HOME/.cargo/bin &&
-export PATH=$PATH:$HOME/.rustup &&
-echo "export PATH=$PATH:$HOME/.cargo/bin" >> ~/.zshrc &&
+export PATH=$PATH:$HOME/.cargo/bin/ &&
+export PATH=$PATH:$HOME/.rustup/ &&
+echo "export PATH=$PATH:$HOME/.cargo/bin/" >> ~/.profile &&
+echo "export PATH=$PATH:$HOME/.cargo/bin/" >> ~/.zprofile &&
+echo "export PATH=$PATH:$HOME/.cargo/bin/" >> ~/.bash_profile &&
 
-# source $HOME/.profile     # have to check why I added this source file command here
-
+# Go headless unattended install Go version 1.20.2
 mkdir -p $HOME/Documents/tools/prog_langs/src &&
 cd $HOME/Documents/tools/prog_langs/src &&
-wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz &&
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.1* &&
-export PATH=$PATH:/usr/local/go/bin &&
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshenv &&
+wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz &&
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20* &&
+export PATH=$PATH:/usr/local/go/bin/ &&
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zprofile &&
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile &&
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bash_profile &&
 cd  &&
+sleep 3 &&
 
 # NVM headless installation
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash &&
 source $HOME/.zshrc &&
+source $HOME/.bashrc &&
 
 # NPM installation and setting the version
-nvm install --lts
-nvm use --lts
+nvm install --lts &&
+nvm use --lts &&
 # nvm install 'lts/*' --reinstall-packages-from=default --latest-npm
 
 # Yarn installation with npm
 npm install --global yarn
-
-# Commented out installation via cargo, might be a better practice to source install
-#cargo install fd-find du-dust lscolors vivid bat hexyl xplr ripgrep arp-scan navi
