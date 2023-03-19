4d3222c13658d9da69615e2ff4642aee93bf89db
Robert Paulsen <ozymandias.1987@protonmail.com>
2023-03-18 23:30:30 -0600

    Finished adding the auto installation of all the ARM and AMD repositories and drivers.

--- repo_installARM.sh
+++ repo_installARM.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
-#
-# List of repos I'm testing or have become standard tools
+# Download tool repositories and drivers repositories and install each of themm.
 
-git clone https://github.com/Botspot/pi-apps.git /home/$USER/Documents/tools &&
-git clone https://github.com/jmcerrejon/PiKISS.git /home/$USER/Documents/tools &&
-git clone https://github.com/sayanarijit/xplr.git /home/$USER/Documents/tools &&
-git clone https://github.com/sharkdp/bat.git /home/$USER/Documents/tools &&
-git clone https://github.com/sharkdp/fd.git /home/$USER/Documents/tools &&
-git clone https://github.com/sharkdp/vivid.git /home/$USER/Documents/tools &&
-git clone https://github.com/Peltoche/lsd.git /home/$USER/Documents/tools &&
-git clone https://github.com/mrjackwills/oxker.git /home/$USER/Documents/tools &&
-git clone https://github.com/denisidoro/navi.git /home/$USER/Documents/tools &&
-git clone https://github.com/rust-lang/mdBook.git /home/$USER/Documents/tools &&
-git clone https://github.com/bootandy/dust.git /home/$USER/Documents/tools &&
-git clone https://github.com/BurntSushi/ripgrep.git /home/$USER/Documents/tools &&
-git clone https://github.com/zcot/conky-manager2.git &&
-git clone https://github.com/MX-Linux/mx-conky-data.git &&
-git clone https://github.com/erikdubois/Aureola.git &&
-git clone https://github.com/brndnmtthws/conky.git &&
-git clone https://github.com/morrownr/8814au.git /home/$USER/.src/drivers &&
-git clone https://github.com/morrownr/8821au-20210708.git /home/$USER/.src/drivers
+git clone https://github.com/sayanarijit/xplr.git $HOME/Documents/tools/xplr &&
+git clone https://github.com/sharkdp/bat.git $HOME/Documents/tools/bat &&
+git clone https://github.com/sharkdp/fd.git $HOME/Documents/tools/fd &&
+git clone https://github.com/sharkdp/vivid.git $HOME/Documents/tools/vivid &&
+git clone https://github.com/Peltoche/lsd.git $HOME/Documents/tools/lsd &&
+git clone https://github.com/mrjackwills/oxker.git $HOME/Documents/tools/oxker &&
+git clone https://github.com/denisidoro/navi.git $HOME/Documents/tools/navi &&
+git clone https://github.com/rust-lang/mdBook.git $HOME/Documents/tools/mdBook &&
+git clone https://github.com/bootandy/dust.git $HOME/Documents/tools/dust &&
+git clone https://github.com/BurntSushi/ripgrep.git $HOME/Documents/tools/ripgrep &&
+git clone https://github.com/morrownr/8814au.git $HOME/.src/drivers/8814au &&
+git clone https://github.com/morrownr/8821au-20210708.git $HOME/.src/drivers/8821au-20210708 &&
+
+# Individual tool installation, more than 95% are rust repositories
+
+cd $HOME/Documents/tools/xplr/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/xplr/ &&
+cd $HOME/Documents/tools/bat/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/bat/ &&
+cd $HOME/Documents/tools/fd/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/fd/ &&
+cd $HOME/Documents/tools/vivid/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/vivid/ &&
+cd $HOME/Documents/tools/lsd/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/lsd/ &&
+cd $HOME/Documents/tools/oxker/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/oxker/ &&
+cd $HOME/Documents/tools/navi/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/navi/ &&
+cd $HOME/Documents/tools/mdBook/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/mdBook/ &&
+cd $HOME/Documents/tools/dust/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/dust/ &&
+cd $HOME/Documents/tools/ripgrep/ &&
+cargo update &&
+cargo install --enable-all-features --path=$HOME/Documents/tools/ripgrep/ &&
+cd
+
+## ---> cd $HOME/.src/drivers/8814au/ && # have to further tink around with the installation to make it more automated
+## ---> cd $HOME/.src/drivers/8821au-20210708 # have to further tink around with the installation to make it more automated
