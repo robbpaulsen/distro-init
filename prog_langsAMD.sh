326549966e40690fb7b586da99128658dea50ce8
Robert Paulsen <ozymandias.1987@protonmail.com>
2023-03-18 23:12:25 -0600

    Poished the programming languagues script for the ARM, AMD and the general example file.

--- prog_langsAMD.sh
+++ prog_langsAMD.sh
@@ -1,34 +1,29 @@
 #!/usr/bin/bash
+\# Script for a more unattended, headless setup and configurationg for the basic things on any new linux installation.
+\# This will cover Directory tree layout, code programming languages framework, some hardware drivers and repositories.
 
-# Scripts to auto download, create direcotries, setup and config all in one,  bash script Golang, rustlang, cargo, nvm, npm, node,yarn, and other tools and pkg.
-
-# Rust and Cargo unattended isntallation
-cd &&
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh -s -- -y --profile default --no-modify-path --default-toolchain nightly &&
+# Rust and Cargo unattended installation
+cd && 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh -s -- -y --profile default --no-modify-path --default-toolchain stable &&
 
 # Adding Cargo tu environment and then setting up local cargo call
-export PATH=$PATH:$HOME/.cargo/bin &&
-export PATH=$PATH:$HOME/.rustup &&
+export PATH=$PATH:/home/$USER/.cargo/bin &&
+export PATH=$PATH:/home/$USER/.rustup &&
+# --> echo "export PATH=$PATH:$HOME/.cargo/bin" >> ~/.zshrc &&   # Podemos ahorrarnos problemas con el PATH si solo seguimos la syntaxis del archivo de "~/.profile" y solo al final de las instalaciones borrar el presente y colocar el propio
 
 ---
-# --> echo "export PATH=$PATH:$HOME/.cargo/bin" >> ~/.zshrc &&
----
+# --> source $HOME/.profile     # have to check why I added this source file command here
 
-# source $HOME/.profile     # have to check why I added this source file command here
-
-mkdir -p $HOME/Documents/tools/prog_langs/src &&
-cd $HOME/Documents/tools/prog_langs/src &&
+# Golang unattended installation
+cd $HOME/.src/prog_langs/ &&
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz &&
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2* &&
 export PATH=$PATH:/usr/local/go/bin &&
 export PATH=$PATH:/home/$USER/go/bin &&
-
----
 # --> echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshenv &&
 # --> echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zprofile &&
----
-
 cd  &&
+---
 
 # NVM headless installation
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash &&
@@ -45,6 +40,4 @@ nvm use --lts &&
 
 # Yarn installation with npm
 npm install --global yarn
-
 # Commented out installation via cargo, might be a better practice to source install
-#cargo install fd-find du-dust lscolors vivid bat hexyl xplr ripgrep arp-scan navi
